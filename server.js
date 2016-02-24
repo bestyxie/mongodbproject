3909affe17e14423cbe0a8562c27a5754017246d
bestyxie <bestyxiejiayun@gmail.com>
2016-02-24 21:30:22 +0800

    database

--- server.js
+++ server.js
@@ -1,35 +1,94 @@
 var express = require('express');
+var path = require('path');
 var port = process.env.PORT || 3000;
 var app = express();
+var bodyParser = require('body-parser');
 
-app.set('views','./views');
+app.set('views','./views/pages');
 app.set('view engine','jade');
+app.use(bodyParser());
+app.use(express.static(path.join(__dirname, 'bower_components')));
 app.listen(port);
 console.log('start on port'+port);
 
 // index page
 app.get('/',function(req,res){
 	res.render('index',{
-		title:'首页'
+		title:'首页',
+		movies: [{
+			title: '机械战警',
+			_id: 1,
+			poster: 'http://r3.ykimg.com/05160000530EEB63675839160D0B79D5'
+		},{
+			title: '机械战警',
+			_id: 2,
+			poster: 'http://r3.ykimg.com/05160000530EEB63675839160D0B79D5'
+		},{
+			title: '机械战警',
+			_id: 3,
+			poster: 'http://r3.ykimg.com/05160000530EEB63675839160D0B79D5'
+		},{
+			title: '机械战警',
+			_id: 4,
+			poster: 'http://r3.ykimg.com/05160000530EEB63675839160D0B79D5'
+		},{
+			title: '机械战警',
+			_id: 5,
+			poster: 'http://r3.ykimg.com/05160000530EEB63675839160D0B79D5'
+		},{
+			title: '机械战警',
+			_id: 6,
+			poster: 'http://r3.ykimg.com/05160000530EEB63675839160D0B79D5'
+		}]	
 	});
 });
 // list
-app.get('/list',function(req,res){
+app.get('/admin/list',function(req,res){
 	res.render('list',{
-		title:'列表页'
+		title:'列表页',
+		movies:[{
+			doctor: '何塞·帕迪利亚',
+			country: '美国',
+			title: '机械战警',
+			year: 2014,
+			poster: 'http://r3.ykimg.com/05160000530EEB63675839160D0B79D5',
+			language: '英语',
+			flash: 'http://player.youku.com/player.php/sid/XNjA1Njc0NTUy/v.swf',
+			summary: '翻拍自1987年同名科幻经典、由《精英部队》导演何塞·帕迪利亚指导的新版《机械战警》发布首款预告片。大热美剧《谋杀》男星乔尔·金纳曼化身新“机械战警”酷黑战甲亮相，加里·奥德曼、塞缪尔·杰克逊、迈克尔·基顿三大戏骨绿叶护航。'
+		}]
 	});
 });
 
 // detail
-app.get('/detail:id',function(req,res){
+app.get('/movie/:id',function(req,res){
 	res.render('detail',{
-		title:'详情页'
+		title:'详情页',
+		movie:{
+			doctor: '何塞·帕迪利亚',
+			country: '美国',
+			title: '机械战警',
+			year: 2014,
+			poster: 'http://r3.ykimg.com/05160000530EEB63675839160D0B79D5',
+			language: '英语',
+			flash: 'http://player.youku.com/player.php/sid/XNjA1Njc0NTUy/v.swf',
+			summary: '翻拍自1987年同名科幻经典、由《精英部队》导演何塞·帕迪利亚指导的新版《机械战警》发布首款预告片。大热美剧《谋杀》男星乔尔·金纳曼化身新“机械战警”酷黑战甲亮相，加里·奥德曼、塞缪尔·杰克逊、迈克尔·基顿三大戏骨绿叶护航。'
+		}
 	});
 });
 
 // admin
 app.get('/admin',function(req,res){
-	res.render('index',{
-		title:'后台管理页面'
+	res.render('admin',{
+		title:'后台管理页面',
+		movie:{
+			title:'',
+			doctor: '',
+			country: '',
+			year: '',
+			poster: '',
+			flash: '',
+			summary: '',
+			language: ''
+		}
 	});
 });
